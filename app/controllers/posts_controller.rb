9373979984f96b99ca48b5d0fad596914c887041
Alon Zuman <alonzuman7@gmail.com>
2020-05-08 11:59:03 +0300

    layout changes and cloudinary

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,25 @@
 class PostsController < ApplicationController
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(set_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end 
+  end
+
+  private
+
+  def set_params
+    params.require(:post).permit(:description) #TODO add the video as well
+  end
 end
