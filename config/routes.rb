06a9760aff88ca45600eda254e8c0ced8fabe2de
Alon Zuman <alonzuman7@gmail.com>
2020-05-10 21:12:08 +0300

    a lot of retouches and discover page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'posts#index'
   get '/me', to: 'users#dashboard', as: 'me'
   resources :posts
+  get '/discover', to:'posts#discover', as: 'discover'
   get '/users/:id', to: 'users#show', as: 'users' # TODO make it auto redirect to '/me' when :id == current_user[:id]
   get '/users/:id/follow', to: 'users#follow', as: 'follow'
   get '/users/:id/unfollow', to: 'users#unfollow', as: 'unfollow'
